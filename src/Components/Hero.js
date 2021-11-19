23b4b9b9be3b52ee6dea9338171463bac00dfec1
frusciante189 <53580986+frusciante189@users.noreply.github.com>
2021-11-20 02:02:40 +0300

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -42,7 +42,7 @@ const Hero = () => {
   return (
     <div className="h-99 relative ">
       <img src="" className="bg-banner bg-cover bg-top h-full" alt="" />
-      <div className="text-white flex flex-col items-center justify-center pt-20 absolute lg:bottom-8 bottom-3 inset-x-0 max-w-[500px] mx-auto">
+      <div className="text-white flex flex-col items-center justify-center pt-20 absolute lg:bottom-10 bottom-3 left-1/2 ml-4 -translate-x-1/2  max-w-[500px] mx-auto">
         <h1 className="text-2xl uppercase font-semibold mt-2 text-white bg-primary text-center w-full">
           Launching on December 19th
         </h1>
